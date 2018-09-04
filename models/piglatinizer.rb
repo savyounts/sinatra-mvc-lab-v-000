1092c14300e6b3f4efbe05eb734ff8db31d45e34
Sav Younts <savyounts@gmail.com>
2018-09-04 19:36:24 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,24 @@
+class PigLatinizer
+  attr_reader :text
+
+  def initialize (text)
+    @text = text
+  end
+
+  def piglatinize
+    words = self.text.split(" ")
+    word.collect do |word|
+      letters = word.split(/[aeiou]/)
+      if letters[0] == (/[aeiou]/)
+        letters << "way"
+        letters.join
+      else
+        last = letters.pop
+        letters << last
+        letters << "way"
+        letters.join
+      end
+    end.join(" ")
+  end 
+
+end
