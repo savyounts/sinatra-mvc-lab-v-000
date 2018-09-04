397ea6a2887fc5bd3959f7e71712f71a01c4486e
Sav Younts <savyounts@gmail.com>
2018-09-04 20:17:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    @translation = PigLatinizer.new(params[:user_phrase])
+    @translation = PigLatinizer.new
 
     erb :piglatinize
   end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,9 @@
 class PigLatinizer
   attr_reader :text
 
+  def initialize(text)
+    @text = text
+  end
 
   def piglatinize
     words = text.split(" ")
